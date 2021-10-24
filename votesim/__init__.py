8578ffa17fc34badc1e89c85fb3db20b49e0b393
Julius <julius.simonelli@gmail.com>
2021-10-24 14:16:59 -0700

    add_head_to_head

--- votesim/__init__.py
+++ votesim/__init__.py
@@ -1,6 +1,7 @@
 from votesim.models import Candidate, Ballot
 from votesim.single_seat_ranking_methods import instant_runoff_voting
 from votesim.multiple_seat_ranking_methods import single_transferable_vote, preferential_block_voting
+from votesim.head_to_head_election import head_to_head
 
 __version__ = "2.0.2"
 
@@ -10,4 +11,5 @@ __all__ = [
     "instant_runoff_voting",
     "single_transferable_vote",
     "preferential_block_voting",
+    "head_to_head",
 ]
